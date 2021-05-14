a5d34c428e3dd49f39c154315c4b1f81966acefe
SondosNasrallah <sondos.nasrallah98@gmail.com>
2021-05-14 20:13:25 +0300

    Final Edition

--- js/app.js
+++ js/app.js
@@ -12,173 +12,195 @@ const hours=['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
     avgCok: 6.3,
     cusEachHr: [],
     cokEachHr: [], 
-    allcookieshops: 0,
+    total: 0,
 
     // # of Customers per hour
-    numCusHr: function () {
-        for (let i = 0; i< hours.length; i++) {
+    
+     calcnumCusHr: function () {
+        for (let i = 0; i < hours.length; i++) {
             // to push random values into the empty cokEachHr arrary
-            this.cusEachHr.push(random( this.minCus, this.maxCus));
-            
-            
+            this.cusEachHr.push(random(this.minCus, this.maxCus));      
         }
     },
-    numCokHr: function () {
+     calcnumCokHr: function () {
      for (let i = 0; i < hours.length; i++) {
          this.cokEachHr.push(Math.floor(this.cusEachHr[i] * this.avgCok)); 
-         this.allcookieshops += this.cokEachHr[i];
+         this.total += this.cokEachHr[i]; //Total
       }
 
     },
     render : function() {
-     let Parent= document.getElementById('firstitem');
-     console.log(Parent);
 
-     let aboutShop = document.createElement('h2');
-     Parent.appendChild(aboutShop); 
-     aboutShop.textcontent=this.cityName;
+        let parent = document.getElementById("parent"); 
+        console.log(parent);
+        let header2 = document.createElement("h2"); 
+
+         parent.appendChild(header2); 
+
+         header2.textContent = this.cityName; 
+
+     
+
 
      // create UL
      let unorderedList = document.createElement('ul');
-     Parent.appendChild(unorderedList);
+     parent.appendChild(unorderedList);
 
      for (let i = 0; i < hours.length; i++) {
          let listItem = document.createElement('li');
          unorderedList.appendChild(listItem);
-         
-      listItem.textcontent= `Total: ${this.allcookieshops} piece of cookies.`   
+         listItem.textContent= `${hours[i]}: ${this.cokEachHr[i]} piece of cookies.`   
 
          
      }
+     let totalItem = document.createElement('li');
+     unorderedList.appendChild(totalItem);
+     totalItem.textContent = `Total: ${this.total} cookies`
 
     }
 }
 
 console.log(seattle);
-seattle.numCusHr();
-seattle.numCokHr(); 
+seattle.calcnumCusHr();
+seattle.calcnumCokHr(); 
 seattle.render();
 
 
 
 /* ****************************************************************************************************************** */
 
-// Tokyo 
-    const tokyo = {
-        cityName:'Tokyo',
-        minCus: 3,
-        maxCus: 24,
-        avgCok: 1.2,
-        cusEachHr: [],
-        cokEachHr: [], 
-        allcookieshops: 0,
+// // Tokyo 
+  const tokyo = {
+    cityName:'Tokyo',
+    minCus: 3,
+    maxCus: 24,
+    avgCok: 1.2,
+    cusEachHr: [],
+    cokEachHr: [], 
+    total: 0,
+
+    // # of Customers per hour
     
-        // # of Customers per hour
-        numCusHr: function () {
-            for (let i = 0; i< hours.length; i++) {
-                // to push random values into the empty cokEachHr arrary
-                this.cusEachHr.push(random( this.minCus, this.maxCus));
-                
-                
-            }
-        },
-        numCokHr: function () {
-         for (let i = 0; i < hours.length; i++) {
-             this.cokEachHr.push(Math.floor(this.cusEachHr[i] * this.avgCok)); 
-             this.allcookieshops += this.cokEachHr[i];
-          }
-    
-        },
-        render : function() {
-         let Parent= document.getElementById('firstitem');
-         console.log(Parent);
-    
-         let aboutShop = document.createElement('h2');
-         Parent.appendChild(aboutShop); 
-         aboutShop.textcontent=this.cityName;
-    
-         // create UL
-         let unorderedList = document.createElement('ul');
-         Parent.appendChild(unorderedList);
-    
-         for (let i = 0; i < hours.length; i++) {
-             let listItem = document.createElement('li');
-             unorderedList.appendChild(listItem);
-             
-          listItem.textcontent= `Total: ${this.allcookieshops} piece of cookies.`   
-    
-             
-         }
-    
+     calcnumCusHr: function () {
+        for (let i = 0; i < hours.length; i++) {
+            // to push random values into the empty cokEachHr arrary
+            this.cusEachHr.push(random(this.minCus, this.maxCus));      
         }
+    },
+     calcnumCokHr: function () {
+     for (let i = 0; i < hours.length; i++) {
+         this.cokEachHr.push(Math.floor(this.cusEachHr[i] * this.avgCok)); 
+         this.total += this.cokEachHr[i]; //Total
+      }
+
+    },
+    render : function() {
+
+        let parent = document.getElementById("parent"); 
+        console.log(parent);
+        let header2 = document.createElement("h2"); 
+
+         parent.appendChild(header2); 
+
+         header2.textContent = this.cityName; 
+
+     
+
+
+     // create UL
+     let unorderedList = document.createElement('ul');
+     parent.appendChild(unorderedList);
+
+     for (let i = 0; i < hours.length; i++) {
+         let listItem = document.createElement('li');
+         unorderedList.appendChild(listItem);
+         listItem.textContent= `${hours[i]}: ${this.cokEachHr[i]} piece of cookies.`   
+
+         
+     }
+     let totalItem = document.createElement('li');
+     unorderedList.appendChild(totalItem);
+     totalItem.textContent = `Total: ${this.total} cookies`
+
     }
-    
-    console.log(tokyo);
-    tokyo.numCusHr();
-    tokyo.numCokHr(); 
-    tokyo.render();
+}
+
+console.log(tokyo);
+tokyo.calcnumCusHr();
+tokyo.calcnumCokHr(); 
+tokyo.render();
+
   
-   /* ************************************************************************************************************** */
+//    /* ************************************************************************************************************** */
    
-   // Dubai
-   const dubai = {
+//    // Dubai
+const dubai = {
     cityName:'Dubai',
     minCus: 11,
     maxCus: 38,
     avgCok: 3.7,
     cusEachHr: [],
     cokEachHr: [], 
-    allcookieshops: 0,
+    total: 0,
 
     // # of Customers per hour
-    numCusHr: function () {
-        for (let i = 0; i< hours.length; i++) {
+    
+     calcnumCusHr: function () {
+        for (let i = 0; i < hours.length; i++) {
             // to push random values into the empty cokEachHr arrary
-            this.cusEachHr.push(random( this.minCus, this.maxCus));
-            
-            
+            this.cusEachHr.push(random(this.minCus, this.maxCus));      
         }
     },
-    numCokHr: function () {
+     calcnumCokHr: function () {
      for (let i = 0; i < hours.length; i++) {
          this.cokEachHr.push(Math.floor(this.cusEachHr[i] * this.avgCok)); 
-         this.allcookieshops += this.cokEachHr[i];
+         this.total += this.cokEachHr[i]; //Total
       }
 
     },
     render : function() {
-     let Parent= document.getElementById('firstitem');
-     console.log(Parent);
 
-     let aboutShop = document.createElement('h2');
-     Parent.appendChild(aboutShop); 
-     aboutShop.textcontent=this.cityName;
+        let parent = document.getElementById("parent"); 
+        console.log(parent);
+        let header2 = document.createElement("h2"); 
+
+         parent.appendChild(header2); 
+
+         header2.textContent = this.cityName; 
+
+     
+
 
      // create UL
      let unorderedList = document.createElement('ul');
-     Parent.appendChild(unorderedList);
+     parent.appendChild(unorderedList);
 
      for (let i = 0; i < hours.length; i++) {
          let listItem = document.createElement('li');
          unorderedList.appendChild(listItem);
-         
-      listItem.textcontent= `Total: ${this.allcookieshops} piece of cookies.`   
+         listItem.textContent= `${hours[i]}: ${this.cokEachHr[i]} piece of cookies.`   
 
          
      }
+     let totalItem = document.createElement('li');
+     unorderedList.appendChild(totalItem);
+     totalItem.textContent = `Total: ${this.total} cookies`
 
     }
 }
 
 console.log(dubai);
-tokyo.numCusHr();
-dubai.numCokHr(); 
+dubai.calcnumCusHr();
+dubai.calcnumCokHr(); 
 dubai.render();
 
 
-/* ****************************************************************************************** */
 
-// Paris
+
+// /* ****************************************************************************************** */
+
+
+// // Paris
 const paris = {
     cityName:'Paris',
     minCus: 20,
@@ -186,109 +208,121 @@ const paris = {
     avgCok: 2.3,
     cusEachHr: [],
     cokEachHr: [], 
-    allcookieshops: 0,
+    total: 0,
 
     // # of Customers per hour
-    numCusHr: function () {
-        for (let i = 0; i< hours.length; i++) {
+    
+     calcnumCusHr: function () {
+        for (let i = 0; i < hours.length; i++) {
             // to push random values into the empty cokEachHr arrary
-            this.cusEachHr.push(random( this.minCus, this.maxCus));
-            
-            
+            this.cusEachHr.push(random(this.minCus, this.maxCus));      
         }
     },
-    numCokHr: function () {
+     calcnumCokHr: function () {
      for (let i = 0; i < hours.length; i++) {
          this.cokEachHr.push(Math.floor(this.cusEachHr[i] * this.avgCok)); 
-         this.allcookieshops += this.cokEachHr[i];
+         this.total += this.cokEachHr[i]; //Total
       }
 
     },
     render : function() {
-     let Parent= document.getElementById('firstitem');
-     console.log(Parent);
 
-     let aboutShop = document.createElement('h2');
-     Parent.appendChild(aboutShop); 
-     aboutShop.textcontent=this.cityName;
+        let parent = document.getElementById("parent"); 
+        console.log(parent);
+        let header2 = document.createElement("h2"); 
+
+         parent.appendChild(header2); 
+
+         header2.textContent = this.cityName; 
+
+     
+
 
      // create UL
      let unorderedList = document.createElement('ul');
-     Parent.appendChild(unorderedList);
+     parent.appendChild(unorderedList);
 
      for (let i = 0; i < hours.length; i++) {
          let listItem = document.createElement('li');
          unorderedList.appendChild(listItem);
-         
-      listItem.textcontent= `Total: ${this.allcookieshops} piece of cookies.`   
+         listItem.textContent= `${hours[i]}: ${this.cokEachHr[i]} piece of cookies.`   
 
          
      }
+     let totalItem = document.createElement('li');
+     unorderedList.appendChild(totalItem);
+     totalItem.textContent = `Total: ${this.total} cookies`
 
     }
 }
 
 console.log(paris);
-paris.numCusHr();
-paris.numCokHr(); 
+paris.calcnumCusHr();
+paris.calcnumCokHr(); 
 paris.render();
 
-/* ******************************************************************************************************************* */
-
-// Lima
+// /* ****************************************************************************************** */
+// // Lima
 const lima = {
     cityName:'Lima',
-    minCus: 3,
-    maxCus: 24,
-    avgCok: 1.2,
+    minCus: 23,
+    maxCus: 65,
+    avgCok: 6.3,
     cusEachHr: [],
     cokEachHr: [], 
-    allcookieshops: 0,
+    total: 0,
 
     // # of Customers per hour
-    numCusHr: function () {
-        for (let i = 0; i< hours.length; i++) {
+    
+     calcnumCusHr: function () {
+        for (let i = 0; i < hours.length; i++) {
             // to push random values into the empty cokEachHr arrary
-            this.cusEachHr.push(random( this.minCus, this.maxCus));
-            
-            
+            this.cusEachHr.push(random(this.minCus, this.maxCus));      
         }
     },
-    numCokHr: function () {
+     calcnumCokHr: function () {
      for (let i = 0; i < hours.length; i++) {
          this.cokEachHr.push(Math.floor(this.cusEachHr[i] * this.avgCok)); 
-         this.allcookieshops += this.cokEachHr[i];
+         this.total += this.cokEachHr[i]; //Total
       }
 
     },
     render : function() {
-     let Parent= document.getElementById('firstitem');
-     console.log(Parent);
 
-     let aboutShop = document.createElement('h2');
-     Parent.appendChild(aboutShop); 
-     aboutShop.textcontent=this.cityName;
+        let parent = document.getElementById("parent"); 
+        console.log(parent);
+        let header2 = document.createElement("h2"); 
+
+         parent.appendChild(header2); 
+
+         header2.textContent = this.cityName; 
+
+     
+
 
      // create UL
      let unorderedList = document.createElement('ul');
-     Parent.appendChild(unorderedList);
+     parent.appendChild(unorderedList);
 
      for (let i = 0; i < hours.length; i++) {
          let listItem = document.createElement('li');
          unorderedList.appendChild(listItem);
-         
-      listItem.textcontent= `Total: ${this.allcookieshops} piece of cookies.`   
+         listItem.textContent= `${hours[i]}: ${this.cokEachHr[i]} piece of cookies.`   
 
          
      }
+     let totalItem = document.createElement('li');
+     unorderedList.appendChild(totalItem);
+     totalItem.textContent = `Total: ${this.total} cookies`
 
     }
 }
 
 console.log(lima);
-lima.numCusHr();
-lima.numCokHr(); 
+lima.calcnumCusHr();
+lima.calcnumCokHr(); 
 lima.render();
+
 
 
 function random (min,max){
